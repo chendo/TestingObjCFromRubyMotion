3c8cc8d61020161f41414c5838f5cd19043e1e33
Jack Chen (chendo) <github@chen.do>
2013-05-25 13:31:48 +1000

    Initial Commit

---
+++ TestingObjCFromRubyMotion/CDOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CDOAppDelegate.h
+//  TestingObjCFromRubyMotion
+//
+//  Created by Jack Chen on 25/05/13.
+//  Copyright (c) 2013 chendo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CDOAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TestingObjCFromRubyMotion/CDOAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  CDOAppDelegate.m
+//  TestingObjCFromRubyMotion
+//
+//  Created by Jack Chen on 25/05/13.
+//  Copyright (c) 2013 chendo. All rights reserved.
+//
+
+#import "CDOAppDelegate.h"
+
+@implementation CDOAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
